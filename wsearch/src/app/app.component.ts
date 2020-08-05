d91d297692db39c98fb9baf0c20112cc06e98b2a
amiyakt <amiya.tripathy1998@gmail.com>
2020-08-05 22:31:53 +0530

    wikipedia search

---
+++ wsearch/src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { WikipediaService } from './wikipedia.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  result = [];
+  constructor(private wikiService: WikipediaService) {}
+
+  onTerm(term: string) {
+    this.wikiService.search(term).subscribe((result: any) => {
+      this.result = result.query.search;
+    });
+  }
+}
